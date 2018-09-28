15f29891bb461fb9d22d54c5206612267137b9d4
michele <mikele.c@outlook.it>
2018-09-28 12:26:02 +0200

    first commit

---
+++ Lab_1/FooCalculator.py
@@ -0,0 +1,13 @@
+import calculator as c
+
+class FooCalculator:
+    def __init__(self):
+        pass
+    
+    def sum(m, n):
+        return c.sum(m, n)
+    
+    def divide(m, n):
+        return c.divide(m, n)
+    
+    
---
+++ Lab_1/calculator.py
@@ -0,0 +1,22 @@
+def sum(m, n):
+    if n<0:
+        m, n = n, m
+    for i in range(n):
+        m += 1
+    return m
+
+
+def divide(m, n):
+    i = -1
+    if n==0:
+        i="infinity"
+    else:
+        sign = -1 if m>0 and n<0 or m<0 and n>0 else 1
+        n, m = abs(n), abs(m)
+    
+        while m>=0:
+            m -= n
+            i += 1
+
+    return i if type(i) == str else i*sign
+    
